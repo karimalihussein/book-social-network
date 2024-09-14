9a9168e132d049bb507bd3392a1da15c6f945b77
Karim Ali Hussein <karimalihussein99@gmail.com>
2024-09-15 01:59:00 +0300

    Refactor TokenRepository interface to add findByToken method

--- src/main/java/com/app/book/user/TokenRepository.java
+++ src/main/java/com/app/book/user/TokenRepository.java
@@ -1,7 +1,8 @@
 package com.app.book.user;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 public interface TokenRepository extends JpaRepository<Token, Integer> {
-   
+    Optional<Token> findByToken(String token);
 }
